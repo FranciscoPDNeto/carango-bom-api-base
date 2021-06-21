935b2bee988a325ca3628ee1cca3392285537ba7
Luiz Galvao <llgalvao24@gmail.com>
2021-06-21 16:54:23 -0300

    chore: remove annotation

--- src/main/java/br/com/caelum/carangobom/marca/MarcaRepository.java
+++ src/main/java/br/com/caelum/carangobom/marca/MarcaRepository.java
@@ -1,11 +1,9 @@
 package br.com.caelum.carangobom.marca;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MarcaRepository extends JpaRepository<Marca, Long> {
 
     List<Marca> findAllByOrderByNome();
