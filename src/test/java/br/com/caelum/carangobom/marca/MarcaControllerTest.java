26b129478c1cd0c74e899856e7c288ce6d9026b0
Luiz Galvao <llgalvao24@gmail.com>
2021-06-22 12:05:52 -0300

    chore: fix tests

--- src/test/java/br/com/caelum/carangobom/marca/MarcaControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/marca/MarcaControllerTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -23,6 +24,7 @@ import static org.mockito.Mockito.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 class MarcaControllerTest {
 
     @Autowired
