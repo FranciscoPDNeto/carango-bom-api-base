1320e15c1067afa25f45c0ae86eadacc88b2cef8
Francisco Dias <franciscopdneto@gmail.com>
2021-06-25 15:09:20 -0300

    issue: wrong annotation on service.

--- src/main/java/br/com/caelum/carangobom/usuario/UsuarioService.java
+++ src/main/java/br/com/caelum/carangobom/usuario/UsuarioService.java
@@ -5,10 +5,8 @@ import br.com.caelum.carangobom.usuario.dtos.UsuarioRequest;
 import br.com.caelum.carangobom.usuario.dtos.UsuarioResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class UsuarioService {
 
     @Autowired
