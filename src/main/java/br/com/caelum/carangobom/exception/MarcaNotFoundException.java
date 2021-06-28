f4b9aa51393f96ad88a600d57fa8280b5bc59b92
Francisco Dias <franciscopdneto@gmail.com>
2021-06-28 15:57:59 -0300

    typo: MarcaNotFoundException.

--- src/main/java/br/com/caelum/carangobom/exception/MarcaNotFoundException.java
+++ src/main/java/br/com/caelum/carangobom/exception/MarcaNotFoundException.java
@@ -2,6 +2,6 @@ package br.com.caelum.carangobom.exception;
 
 public class MarcaNotFoundException extends RuntimeException {
     public MarcaNotFoundException() {
-        super("Marca não encontrado.");
+        super("Marca não encontrada.");
     }
 }
