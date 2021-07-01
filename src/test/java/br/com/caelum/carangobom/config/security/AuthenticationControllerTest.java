e2864064c7c094ddca3970451f5bd64f0b9b627a
Francisco Dias <franciscopdneto@gmail.com>
2021-07-01 19:13:52 -0300

    chore: tests to authenticate.

--- src/test/java/br/com/caelum/carangobom/config/security/AuthenticationControllerTest.java
+++ src/test/java/br/com/caelum/carangobom/config/security/AuthenticationControllerTest.java
@@ -1,52 +1,68 @@
 package br.com.caelum.carangobom.config.security;
 
+import br.com.caelum.carangobom.usuario.UsuarioRepository;
 import br.com.caelum.carangobom.usuario.dtos.UsuarioRequest;
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.net.URI;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@ContextConfiguration
-@WebAppConfiguration
+@AutoConfigureMockMvc
 @ActiveProfiles("test")
 class AuthenticationControllerTest {
 
     @Autowired
-    private WebApplicationContext context;
-
     private MockMvc mvc;
 
-    @BeforeEach
-    public void setup() {
-        mvc = MockMvcBuilders
-            .webAppContextSetup(context)
-            .apply(SecurityMockMvcConfigurers.springSecurity())
-            .build();
-    }
+    @MockBean
+    private UsuarioRepository usuarioRepository;
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     void deveRetornarBadRequestQuandoAutenticateUsuarioInvalido() throws Exception {
+        // given
         var uri = new URI("/auth");
         UsuarioRequest usuarioRequest = new UsuarioRequest("luiz", "123456");
 
-        String json = "{\"username\": \"luiz\", \"password\": \"123456\"}";
+        // when
+        String json = objectMapper.writeValueAsString(usuarioRequest);
+        when(usuarioRepository.findByUsername(usuarioRequest.getUsername())).thenReturn(Optional.empty());
 
+        // then
         mvc
             .perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
             .andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void deveRetornarOkQuandoAutenticateUsuario() throws Exception {
+        // given
+        var uri = new URI("/auth");
+        UsuarioRequest usuarioRequest = new UsuarioRequest("luiz", "123456");
+
+        // when
+        String json = objectMapper.writeValueAsString(usuarioRequest);
+        var usuario = usuarioRequest.toModel();
+        usuario.setId(1L);
+        when(usuarioRepository.findByUsername(usuarioRequest.getUsername())).thenReturn(Optional.of(usuario));
+
+        // then
+        mvc
+            .perform(MockMvcRequestBuilders.post(uri).content(json).contentType(MediaType.APPLICATION_JSON))
+            .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
