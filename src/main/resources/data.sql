c0db7645fb2410e1fb95523699d6dc75ea298785
Francisco Dias <franciscopdneto@gmail.com>
2021-07-01 14:29:41 -0300

    data sql initialization.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO usuario(nome, senha) VALUES('João', '$2a$10$yUz4LCqpSlv9.YnAfOKoB.uFVZHDETRLD/R14S9X9ZV/OLw1EWgN6');
+
+INSERT INTO marca(nome) VALUES('Fiat');
+INSERT INTO marca(nome) VALUES('Volkswagen');
+INSERT INTO marca(nome) VALUES('Ford');
+
+INSERT INTO veiculo(ano, modelo, valor, marca_id) VALUES(2020, "Ecosport", 60000, 2);
+INSERT INTO veiculo(ano, modelo, valor, marca_id) VALUES(2020, "Gol", 50000, 1);
+INSERT INTO veiculo(ano, modelo, valor, marca_id) VALUES(2018, "Gol", 33000, 1);
+INSERT INTO veiculo(ano, modelo, valor, marca_id) VALUES(2020, "Uno", 42000, 0);
