a92f1248c8a4060cc91e4868c3f5ab84ca97e2bc
Francisco Dias <franciscopdneto@gmail.com>
2021-07-02 10:59:04 -0300

    chore: add authorization parameter on swagger.

--- src/main/java/br/com/caelum/carangobom/config/swagger/SwaggerConfigurations.java
+++ src/main/java/br/com/caelum/carangobom/config/swagger/SwaggerConfigurations.java
@@ -1,11 +1,16 @@
 package br.com.caelum.carangobom.config.swagger;
 
+import br.com.caelum.carangobom.usuario.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
 
 @Configuration
 public class SwaggerConfigurations {
@@ -15,7 +20,17 @@ public class SwaggerConfigurations {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.caelum.carangobom"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .ignoredParameterTypes(Usuario.class)
+                .globalOperationParameters(Arrays.asList(
+                    new ParameterBuilder()
+                        .name("Authorization")
+                        .description("Header para toker JWT")
+                        .modelRef(new ModelRef("string"))
+                        .parameterType("header")
+                        .required(false)
+                        .build()
+                ));
     }
 
 }
