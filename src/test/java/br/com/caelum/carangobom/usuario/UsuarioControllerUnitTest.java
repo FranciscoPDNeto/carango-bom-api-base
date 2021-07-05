781eadb082324cd6626d27ca96c6a33737169c3b
Francisco Dias <franciscopdneto@gmail.com>
2021-07-05 11:42:47 -0300

    chore: unit tests to usuario.

--- src/test/java/br/com/caelum/carangobom/usuario/UsuarioControllerUnitTest.java
+++ src/test/java/br/com/caelum/carangobom/usuario/UsuarioControllerUnitTest.java
@@ -1,12 +1,18 @@
 package br.com.caelum.carangobom.usuario;
 
+import br.com.caelum.carangobom.exception.UsuarioAlreadyRegisteredException;
 import br.com.caelum.carangobom.exception.UsuarioNotFoundException;
 import br.com.caelum.carangobom.usuario.dtos.PasswordRequest;
+import br.com.caelum.carangobom.usuario.dtos.UsuarioRequest;
+import br.com.caelum.carangobom.usuario.dtos.UsuarioResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -17,6 +23,8 @@ class UsuarioControllerUnitTest {
 
     private UsuarioController usuarioController;
 
+    private UriComponentsBuilder uriBuilder;
+
     @Mock
     private UsuarioService usuarioService;
 
@@ -25,6 +33,64 @@ class UsuarioControllerUnitTest {
         openMocks(this);
 
         usuarioController = new UsuarioController(usuarioService);
+        uriBuilder = UriComponentsBuilder.fromUriString("http://localhost:8080");
+    }
+
+    @Test
+    void deveCadastrarComSucesso() {
+        var usuarioResponse = new UsuarioResponse(1L, "Jose");
+        when(usuarioService.registerNewUser(any())).thenReturn(usuarioResponse);
+
+        ResponseEntity<UsuarioResponse> response =
+            usuarioController.save(new UsuarioRequest("Jose", "1234"), uriBuilder);
+
+        assertEquals(HttpStatus.CREATED, response.getStatusCode());
+        assertEquals("http://localhost:8080/usuarios/1", response.getHeaders().getLocation().toString());
+    }
+
+    @Test
+    void deveRetornarBadRequestAoTentarCadastrarUsuarioQueJaExista() {
+        doThrow(UsuarioAlreadyRegisteredException.class).when(usuarioService).registerNewUser(any());
+
+        ResponseEntity<UsuarioResponse> response =
+            usuarioController.save(new UsuarioRequest("Jose", "1234"), uriBuilder);
+
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+    }
+
+    @Test
+    void deveRetornarListaDeUsuariosCadastrados() {
+        var usuarios = List.of(
+            new UsuarioResponse(1L, "João"),
+            new UsuarioResponse(2L, "Maria"),
+            new UsuarioResponse(3L, "José"),
+            new UsuarioResponse(4L, "Ana")
+        );
+
+        when(usuarioService.findAll()).thenReturn(usuarios);
+
+        var response = usuarioController.getAll();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(usuarios, response.getBody());
+    }
+
+    @Test
+    void deveExcluirUsuarioExistente() {
+        doNothing().when(usuarioService).delete(anyLong());
+
+        var response = usuarioController.delete(1L);
+
+        assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+    }
+
+    @Test
+    void deveRetornarNotFoundAoTentarExcluirUsuarioInexistente() {
+        doThrow(UsuarioNotFoundException.class).when(usuarioService).delete(anyLong());
+
+        var response = usuarioController.delete(1L);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
     }
 
     @Test
--- src/test/java/br/com/caelum/carangobom/usuario/UsuarioServiceTest.java
+++ src/test/java/br/com/caelum/carangobom/usuario/UsuarioServiceTest.java
@@ -1,14 +1,14 @@
 package br.com.caelum.carangobom.usuario;
 
+import br.com.caelum.carangobom.config.security.TokenService;
 import br.com.caelum.carangobom.exception.UsuarioAlreadyRegisteredException;
 import br.com.caelum.carangobom.exception.UsuarioNotFoundException;
 import br.com.caelum.carangobom.usuario.dtos.UsuarioRequest;
 import br.com.caelum.carangobom.usuario.dtos.UsuarioResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
+import org.mockito.Mock;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,33 +16,47 @@ import java.util.Optional;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalToObject;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
-@SpringBootTest
-@ActiveProfiles("test")
 class UsuarioServiceTest {
 
-    @Autowired
     private UsuarioService usuarioService;
 
-    @MockBean
+    @Mock
     private UsuarioRepository repository;
+
+    @Mock
+    private TokenService tokenService;
+
+    @BeforeEach
+    public void configure() {
+        openMocks(this);
+
+        usuarioService = new UsuarioService(repository, tokenService);
+
+        when(tokenService.isValidToken(anyString())).thenReturn(true);
+    }
 
     @Test
     void deveCadastrarNovoUsuario() {
         // given
         var usuarioRequest = new UsuarioRequest("nomeTeste", "12345");
-        var usuarioFromRequest = usuarioRequest.toModel();
-        usuarioFromRequest.setId(1L);
 
         // when
         when(repository.findByUsername(usuarioRequest.getUsername())).thenReturn(Optional.empty());
-        when(repository.save(any())).thenReturn(usuarioFromRequest);
+        when(repository.save(any(Usuario.class))).then(invocation -> {
+            var usuario = invocation.getArgument(0, Usuario.class);
+            usuario.setId(1L);
+
+            return usuario;
+        });
 
         // then
         var usuarioResponse = usuarioService.registerNewUser(usuarioRequest);
@@ -104,5 +118,33 @@ class UsuarioServiceTest {
         // then
         assertThrows(UsuarioNotFoundException.class, () -> usuarioService.delete(1L));
         verify(repository, never()).delete(any());
+    }
+
+    @Test
+    void deveAtualizarSenhaDoUsuario() {
+        final var oldPassword = "1234";
+        final var usuario = new Usuario(1L, "Joao", oldPassword);
+        final var newPassword = "123456";
+        final var token =
+            ".eyJpc3MiOiJBUEkgZG8gQ2FyYW5nb0JvbSIsInN1YiI6IjgiLCJpYXQiOjE2MjUyNDgwMjksImV4cCI6MTYyNTMzNDQyOX0.tlX9KM1aLUjA_3w7oChzjqsUzDh4rh2vUR-rmYwuyZs";
+
+        when(tokenService.getUserId(token)).thenReturn(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(usuario));
+
+        usuarioService.updatePassword(newPassword, token);
+
+        assertFalse(BCrypt.checkpw(oldPassword, usuario.getPassword()));
+        assertTrue(BCrypt.checkpw(newPassword, usuario.getPassword()));
+    }
+
+    @Test
+    void naoDeveAtualizarSenhaDeUsuarioInexistente() {
+        final var token =
+            ".eyJpc3MiOiJBUEkgZG8gQ2FyYW5nb0JvbSIsInN1YiI6IjgiLCJpYXQiOjE2MjUyNDgwMjksImV4cCI6MTYyNTMzNDQyOX0.tlX9KM1aLUjA_3w7oChzjqsUzDh4rh2vUR-rmYwuyZs";
+
+        when(tokenService.getUserId(token)).thenReturn(1L);
+        when(repository.findById(1L)).thenReturn(Optional.empty());
+
+        assertThrows(UsuarioNotFoundException.class, () -> usuarioService.updatePassword("123456", token));
     }
 }
